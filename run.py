fc503b482d7950523243b845b57aa7ccf17bdd55
U1805 <45514638+U1805@users.noreply.github.com>
2022-01-23 19:16:42 +0800

    🌕

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
-import cv2
+import sys,os,re
 import pysubs2
-import re
+import cv2
+import threading
 
-def Inpainting(srcImg,x,y,xx,yy,style="文本",kernelSize=7,iter=1,r=4):
+def Inpainting(srcImg,x,y,xx,yy,style="文本",kernelSize=7,iter=1,r=3):
     src = srcImg[x:xx,y:yy].copy()
     gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY,1)
     if style == "学生" or style == "文本": #白字
@@ -14,7 +15,33 @@ def Inpainting(srcImg,x,y,xx,yy,style="文本",kernelSize=7,iter=1,r=4):
     inpaintImg = cv2.inpaint(src,maskImg,r,cv2.INPAINT_NS)
     srcImg[x:xx,y:yy] = inpaintImg
 
-filename = input("input filename:")
+# 多线程-任务
+def work(start):
+    video_path = os.path.join("./temp/", "part_"+str(start)+".mp4")
+    cap = cv2.VideoCapture(video_path)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    width = int(cap.get(3))
+    height = int(cap.get(4))
+    out = cv2.VideoWriter("./temp/part_"+str(start)+"_out.mp4",fourcc,fps,(width,height))
+    print("start threading "+str(start))
+
+    for i in range(start*num,(start+1)*num):
+        ret, img = cap.read()
+        if ret:
+            for style in frame[i]:
+                y1 ,x1 ,y2 ,x2= axis[style][0], axis[style][1], axis[style][2], axis[style][3]
+                Inpainting(img,x1,y1,x2,y2,style,kernelSize=9,iter=1,r=4)
+            out.write(img)
+        else:
+            break
+        if cv2.waitKey(1) & 0xFF==27:
+            break
+    cap.release()
+    out.release()
+    print("finish threading "+str(start))
+
+# 坐标
 axis = {}
 with open("config.txt", "r",encoding='utf-8') as f:
     for line in f.readlines():
@@ -23,20 +50,21 @@ with open("config.txt", "r",encoding='utf-8') as f:
         style = line[0]
         axis[style] = list(map(int,list(line[1].split(','))))
 
+filename = input("input filename: ")
+path = sys.path[0]+"\\temp\\"
 cap = cv2.VideoCapture(filename+".mp4")
 length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 fps = cap.get(cv2.CAP_PROP_FPS)
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-
 width = int(cap.get(3))
 height = int(cap.get(4))
-out = cv2.VideoWriter(filename+'_out.mp4',fourcc,fps,(width,height))  
-
 frame = []
 for i in range(length):
-        frame.append([])
+    frame.append([])
+if not os.path.exists(path):
+    os.makedirs(path)
 
-
+# 时间轴标记
 subs = pysubs2.load(filename+".ass")
 for sub in subs:
     start = pysubs2.time.ms_to_frames(sub.start, fps)
@@ -49,10 +77,10 @@ for sub in subs:
         if name_school != text:
             name = name_school.split('（')[0]
             school = name_school.split('（')[-1].replace("）","")
-            subs.insert(0, pysubs2.SSAEvent(start=sub.start, end=sub.end, text=name+" {\\fs35 \c&H f4ca80 }"+school))
+            subs.insert(0, pysubs2.SSAEvent(start=sub.start, end=sub.end, text=name+" {\\fs35 \c&H f4ca80 }"+school, style="学生"))
 
-        if len(text) > 35:
-            text = re.sub(r"(.{35})","\\1\n",text)
+        if len(text) > 35: # 换行
+            text = "\\N".join(re.findall(".{35}",text)) 
         sub.text = text
 
         for i in range(start,end):
@@ -62,24 +90,36 @@ for sub in subs:
     else:
         for i in range(start,end):
             frame[i].append(sub.style)
-subs.save(filename+"_out.ass")
+subs.save(filename + "_out.ass")
 
-
-for i in range(length):
+# 多线程-预处理切分
+num = 1000
+i = -1
+while 1:
+    i += 1
     ret, img = cap.read()
-    print(str(i)+"/"+str(length))
     if ret:
-        for style in frame[i]:
-            y1 ,x1 ,y2 ,x2= axis[style][0], axis[style][1], axis[style][2], axis[style][3]
-            Inpainting(img,x1,y1,x2,y2,style)
+        if i % num == 0:
+            out = cv2.VideoWriter(path + "part_" + str(int(i/num)) + ".mp4",fourcc,fps,(width,height))
         out.write(img)
-        img = cv2.resize(img, (0, 0),fx = 0.5,fy = 0.5)
-        cv2.imshow('img',img)
     else:
-        print("Error")
-        break
-    if cv2.waitKey(1) & 0xFF==27:
         break
 cap.release()
 out.release()
-cv2.destroyAllWindows()
+cnt = -1 * (-i // num)
+with open(path + "list.txt","a") as f:
+    for i in range(0,cnt):
+        f.write("file ./temp/part_"+str(i)+"_out.mp4\n")
+
+thread_list = []
+for i in range(0,cnt):
+    thread = threading.Thread(target=work, args=[i])
+    thread.start()
+    thread_list.append(thread)
+for t in thread_list:
+    t.join()
+
+os.system("ffmpeg -f concat -safe 0 -i {path}list.txt -c copy {path}concat.mp4".format(path=path))
+os.system("ffmpeg -i {path}concat.mp4 -i {filename}.mp4  -c copy -map 0 -map 1:1 -y -shortest {filename}_out.mp4".format(path=path,filename=filename))
+os.system("ffmpeg -i {filename}_out.mp4 -vf subtitles={filename}_out.ass {filename}_final.mp4".format(filename=filename))
+os.system("rd/s/q "+path +" && del {filename}_out.ass {filename}_out.mp4".format(filename=filename))
